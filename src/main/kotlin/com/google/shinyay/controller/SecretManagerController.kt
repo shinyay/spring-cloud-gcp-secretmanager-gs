ce96bbaf28a31fde65783ae5da64b8db0f4985a9
shinyay <shinya.com@gmail.com>
2021-01-12 11:32:45 +0900

    Add current time

--- src/main/kotlin/com/google/shinyay/controller/SecretManagerController.kt
+++ src/main/kotlin/com/google/shinyay/controller/SecretManagerController.kt
@@ -2,11 +2,23 @@ package com.google.shinyay.controller
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @Controller
+@RequestMapping("/api/v1")
 class SecretManagerController {
 
     @Value("\${sm://app-secret}")
     lateinit var appSecret: String
+
+    @GetMapping("hello")
+    fun helloController() {
+        val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
+        val currentTime = ZonedDateTime.now(ZoneId.of("Japan")).format(dateFormat)
+    }
 
 }
