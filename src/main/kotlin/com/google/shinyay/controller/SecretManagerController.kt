d5aaceacec098c6ff0e30dc255dbfb3c8d6170fe
shinyay <shinya.com@gmail.com>
2021-01-12 09:30:02 +0900

    Add variable from Secret Manager

--- src/main/kotlin/com/google/shinyay/controller/SecretManagerController.kt
+++ src/main/kotlin/com/google/shinyay/controller/SecretManagerController.kt
@@ -1,7 +1,12 @@
 package com.google.shinyay.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 
 @Controller
 class SecretManagerController {
+
+    @Value("\${sm://app-secret}")
+    lateinit var appSecret: String
+
 }
